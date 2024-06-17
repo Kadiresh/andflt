541439a9df1b18ea53c94049a2e19566019b0f27
Kadiresh <kadireshn@gmail.com>
2024-06-17 18:49:47 +0530

    andro1.2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello Andro1.2!'),
+          child: Text('Hello Andro1.3!'),
         ),
       ),
     );
